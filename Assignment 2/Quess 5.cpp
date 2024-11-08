9a83c0a32cf365c0b3248c2ef26ea743f0f1245c
BirSingh-1 <bsingh6_be23@thapar.edu>
2024-11-08 12:37:37 +0530

    Quess 7.cpp to Quess 5.cpp

--- Assignment 2/Quess 5.cpp
+++ Assignment 2/Quess 5.cpp
@@ -1,4 +1,4 @@
-//	Let A[1 …. n] be an array of n real numbers. A pair (A[i], A[j ]) is said to be an inversion 
+//	Let A[1 â€¦. n] be an array of n real numbers. A pair (A[i], A[j ]) is said to be an inversion 
 //	if these numbers are out of order, i.e., i < j but A[i]>A[j ]. Write a program to count the 
 //	number of inversions in an array.
 
